f9cdd27e1afc69e9b6ebc3d9057fa20124d396ad
TripuraMetta <tripurassc@gmail.com>
2026-05-12 13:53:15 +0530

    Enabled CORS for frontend

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app)
+    CORS(app, origins=["https://task-workflow-management-system-88d.vercel.app"])
     
     db.init_app(app)
     migrate.init_app(app, db)
